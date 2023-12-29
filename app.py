2a2195383fc0cb9a975f2070ae18c12caed330fb
davidmatos17 <davidmatos0417@gmail.com>
2023-12-29 00:19:06 +0000

    commit

--- app.py
+++ app.py
@@ -37,110 +37,98 @@ def list_routes():
 
     return render_template('routes-list.html', routes=routes)
 
-@APP.route('/routes/A')
-def route_A():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'A'
-      ''').fetchall()
+
+
+@APP.route('/routes/<nome>')
+def route(nome):
+    count= '''SELECT
+    COUNT (*) contador 
+    FROM
+        Paragens
+    JOIN
+        Horarios ON idParagem = paragemID
+    WHERE
+        Replace(linhaID, "Bexp", "Bx") = ?'''
+    
+    query = '''SELECT nome as Name , cor as Color
+                FROM Linhas
+                Where nome = ?
+                '''
+    paragens = '''
+    SELECT
+    nomeDaParagem as Name
+    FROM
+        Paragens
+    JOIN
+        Horarios ON idParagem = paragemID
+    WHERE
+        Replace(linhaID, "Bexp", "Bx") = ?
+    
+    '''
+    routes = db.execute(query,(nome,)).fetchall()
+    stations = db.execute(paragens,(nome,)).fetchall()
+    contador = db.execute(count, (nome,)).fetchall()
     if not routes:
         abort(404)  # or handle it in another way, e.g., render an error template
 
-    return render_template('routes-list.html', routes=routes)
+    return render_template('routes.html', routes=routes, stations= stations, contador= contador)
 
-@APP.route('/routes/B')
-def route_B():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'B'
+ 
 
-      ''').fetchall()
+
+@APP.route('/routes-search/<color>/')
+def color_search(color):
+    # Use a parameterized query to avoid SQL injection
+    query = '''
+        SELECT nome as Name, cor as Color
+        FROM Linhas
+        WHERE cor = ?
+    '''
+    routes = db.execute(query, (color,)).fetchall()
+
     if not routes:
         abort(404)  # or handle it in another way, e.g., render an error template
 
-    return render_template('routes-list.html', routes=routes)   
+    return render_template('routes-search.html', color=color, routes=routes)
 
-@APP.route('/routes/Bx')
-def route_Bx():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'Bx'
 
-      ''').fetchall()
-    if not routes:
-        abort(404)  # or handle it in another way, e.g., render an error template
-
-    return render_template('routes-list.html', routes=routes)
-
-@APP.route('/routes/C')
-def route_C():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'C'
-
-      ''').fetchall()
-    if not routes:
-        abort(404)  # or handle it in another way, e.g., render an error template
-
-    return render_template('routes-list.html', routes=routes)
-
-@APP.route('/routes/D')
-def route_D():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'D'
-      ''').fetchall()
-    if not routes:
-        abort(404)  # or handle it in another way, e.g., render an error template
-
-    return render_template('routes-list.html', routes=routes)
-
-@APP.route('/routes/E')
-def route_E():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'E'
-      ''').fetchall()
-    if not routes:
-        abort(404)  # or handle it in another way, e.g., render an error template
-
-    return render_template('routes-list.html', routes=routes)
-
-@APP.route('/routes/F')
-def route_F():
-    routes = db.execute(
-      '''
-      SELECT nome as Name, cor as Color
-      FROM Linhas
-      WHERE nome = 'F'
-      ''').fetchall()
-    if not routes:
-        abort(404)  # or handle it in another way, e.g., render an error template
-
-    return render_template('routes-list.html', routes=routes)
-
+#  Pagina das paragens
 @APP.route('/stops/')
-def paragens():
+def list_stops():
     stops = db.execute(
-        '''
-        Select nomeDaParagem as Name
-        From Paragens
-        Order by Name
-        ''').fetchall()
-    if not stops:
+    '''
+    SELECT idParagem as ID, zonaIdOrigem as Zone, nomeDaParagem as Name
+    FROM Paragens
+    ORDER BY ID
+    ''').fetchall()
+    return render_template('stops-list.html',stops=stops)
+
+@APP.route('/stops/<ID>/')
+def stop(ID):
+    station = '''SELECT
+    nomeDaParagem AS Name,
+    zonaIdOrigem AS Zone,
+    idParagem AS ID
+    FROM Paragens
+    WHERE ID = ?'''
+    
+    query = '''
+    SELECT
+    REPLACE(linhaID, 'Bexp', 'Bx') AS Route
+FROM
+    Horarios
+JOIN
+    Paragens ON idParagem = paragemID
+WHERE
+    idParagem = ?
+ORDER BY Route;
+
+    '''
+    paragens = db.execute(query, (ID,)).fetchall()
+    stations = db.execute(station, (ID,)).fetchall()
+    if not paragens:
         abort(404)  # or handle it in another way, e.g., render an error template
+<<<<<<< Updated upstream
     return render_template('stops-list.html', stops=stops)
 
 @APP.route('/search-stops/', methods=['POST'])
@@ -157,3 +145,71 @@ def search_stops():
     ).fetchall()
 
     return render_template('stops-list.html', stops=stops)
+=======
+
+    return render_template('stops.html', ID=ID, paragens=paragens, stations = stations)
+
+
+@APP.route('/stops_by_name/<Name>/')
+def stop_search_By_Name(Name):
+    station = '''SELECT
+    nomeDaParagem AS Name,
+    zonaIdOrigem AS Zone,
+    idParagem AS ID
+    FROM Paragens
+    WHERE Name = ?'''
+    
+    query = '''
+    SELECT
+    REPLACE(linhaID, 'Bexp', 'Bx') AS Route
+FROM
+    Horarios
+JOIN
+    Paragens ON idParagem = paragemID
+WHERE
+    nomeDaParagem = ?
+ORDER BY Route;
+
+    '''
+    paragens = db.execute(query, (Name,)).fetchall()
+    stations = db.execute(station, (Name,)).fetchall()
+    if not paragens:
+        abort(404)  # or handle it in another way, e.g., render an error template
+
+    return render_template('stops.html', Name=Name, paragens=paragens, stations = stations)
+
+# Pagina dos tickets
+@APP.route('/tickets/')
+def list_ticket_prices():
+    tickets = db.execute(
+    '''
+    SELECT tituloID as Ticket, preco as Price
+    FROM Titulos
+    GROUP BY Ticket, Price
+    HAVING COUNT(*) > 1
+    ''').fetchall()
+    if not tickets:
+        abort(404)
+    return render_template('tickets-price-list.html', tickets=tickets)
+
+@APP.route('/tickets/<zone>/')
+def search_by_Zone(zone):
+    preco = '''
+               SELECT DISTINCT preco as Price , tituloID as Ticket
+               FROM Titulos
+               WHERE tituloID = ?
+            '''
+    query =  '''
+            Select  tituloID as Ticket,  preco as Price, zonaIdOrigem , zonaIdDestino
+            FROM Titulos
+            Where Ticket = ?
+        '''
+    zonas = db.execute(query, (zone,)).fetchall()
+    precos = db.execute(preco, (zone,)).fetchall()
+    if not (zonas and precos) :
+        abort(404)
+    return render_template('tickets-search.html', precos=precos, zonas=zonas)
+
+
+
+>>>>>>> Stashed changes
